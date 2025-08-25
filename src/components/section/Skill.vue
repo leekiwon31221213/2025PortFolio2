dffb3bc32e95cf51bf2b3bef5afd6e47635a40e1
Lee Ki Won <cute01235@nate.com>
2025-08-25 17:55:03 +0900

    [update] skill부분작업

--- src/components/section/Skill.vue
+++ src/components/section/Skill.vue
@@ -1,13 +1,45 @@
 <template>
   <section id="skill" class="section3">
     <h1 lang="en">Skill & Tool</h1>
-    <p>
+    <p class="desc">
       웹퍼블리싱 기초부터 모던 프론트엔드 기술까지 <br />
       시멘틱 태그를 활용한 기본에 충실한 마크업과 스타일링부터, 최신 프레임워크와 렌더링 기법을
       적용한 현대적인 웹 개발까지 폭넓게 다룹니다.
     </p>
 
     <ul class="skills-box">
+      <li class="front-skill">
+        <ul class="skill-list">
+          <li>
+            <h3>{{ front.title }}</h3>
+          </li>
+          <li v-for="(skill, index) in front.skills" :key="index" :type="skill.type">
+            <p class="skill-name" lang="en">{{ skill.name }}</p>
+            <div class="skill-level">
+              <div class="skill-bar" :style="{ '--target-width': skill.level + '%' }">
+                <span class="skill-text">{{ skill.level }}%</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </li>
+
+      <li class="front-skill">
+        <ul class="skill-list">
+          <li>
+            <h3>{{ front.title }}</h3>
+          </li>
+          <li v-for="(skill, index) in front.skills" :key="index" :type="skill.type">
+            <p class="skill-name" lang="en">{{ skill.name }}</p>
+            <div class="skill-level">
+              <div class="skill-bar" :style="{ '--target-width': skill.level + '%' }">
+                <span class="skill-text">{{ skill.level }}%</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </li>
+
       <li class="front-skill">
         <h3>{{ front.title }}</h3>
 
@@ -62,16 +94,42 @@ export default {
 
 <style lang="scss">
 #skill {
+  width: 90%;
+  margin: 0 auto;
+  h1 {
+    padding: 145px 0 35px 0;
+    text-align: center;
+    margin: 0 auto;
+    font-size: 8rem;
+    font-weight: 600;
+    letter-spacing: 5px;
+  }
+  p.desc {
+    text-align: center;
+    line-height: 3.5rem;
+    font-size: 2rem;
+    color: $dec;
+    font-weight: 200;
+    margin-bottom: 12.8rem;
+  }
   .skills-box {
+    display: flex;
+    justify-content: space-evenly;
+    li {
+      display: flex;
+      width: calc(100% / 3);
+    }
     .front-skill {
       .skill-list {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-
+        width: 100%;
+        margin-right: 5rem;
+        &:nth-of-type(3) {
+          margin-right: 0;
+        }
         > li {
           margin-bottom: 15px;
           display: flex;
+          width: 100%;
 
           .skill-name {
             font-weight: 200;
