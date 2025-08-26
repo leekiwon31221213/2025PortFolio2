16f30c24aef03e14fd3df4c8cc6fd2412d314fa8
Lee Ki Won <cute01235@nate.com>
2025-08-26 17:45:57 +0900

    [update]

--- src/components/section/Skill.vue
+++ src/components/section/Skill.vue
@@ -8,9 +8,13 @@
     </p>
 
     <ul class="skills-box">
-      <li class="front-skill">
+      <!-- 스킬 -->
+      <li class="skill__inner front-skill glass glass">
         <ul class="skill-list">
           <li>
+            <div class="icon-box front">
+              <img :src="front.icon" :alt="front.alt" />
+            </div>
             <h3>{{ front.title }}</h3>
           </li>
           <li v-for="(skill, index) in front.skills" :key="index" :type="skill.type">
@@ -23,34 +27,45 @@
           </li>
         </ul>
       </li>
-
-      <li class="front-skill">
+      <!-- 디자인 -->
+      <li class="skill__inner design-skill glass">
         <ul class="skill-list">
           <li>
-            <h3>{{ front.title }}</h3>
-          </li>
-          <li v-for="(skill, index) in front.skills" :key="index" :type="skill.type">
-            <p class="skill-name" lang="en">{{ skill.name }}</p>
-            <div class="skill-level">
-              <div class="skill-bar" :style="{ '--target-width': skill.level + '%' }">
-                <span class="skill-text">{{ skill.level }}%</span>
-              </div>
+            <div class="icon-box design">
+              <img :src="design.icon" :alt="design.alt" />
             </div>
+            <h3>{{ design.title }}</h3>
+          </li>
+          <li v-for="(skill, index) in design.skills" :key="index">
+            <p class="skill-name" lang="en">{{ skill.name }}</p>
           </li>
         </ul>
       </li>
-
-      <li class="front-skill">
-        <h3>{{ front.title }}</h3>
-
+      <!-- 협업 -->
+      <li class="skill__inner collaboration-skill glass">
         <ul class="skill-list">
-          <li v-for="(skill, index) in front.skills" :key="index" :type="skill.type">
-            <p class="skill-name" lang="en">{{ skill.name }}</p>
-            <div class="skill-level">
-              <div class="skill-bar" :style="{ '--target-width': skill.level + '%' }">
-                <span class="skill-text">{{ skill.level }}%</span>
-              </div>
+          <li>
+            <div class="icon-box collaboration">
+              <img :src="collaboration.icon" :alt="collaboration.alt" />
             </div>
+            <h3>{{ collaboration.title }}</h3>
+          </li>
+          <li v-for="(skill, index) in collaboration.skills" :key="index">
+            <p class="skill-name" lang="en">{{ skill.name }}</p>
+          </li>
+        </ul>
+      </li>
+      <!-- 기타 -->
+      <li class="skill__inner etc-skill glass">
+        <ul class="skill-list">
+          <li>
+            <div class="icon-box etc">
+              <img :src="etc.icon" :alt="etc.alt" />
+            </div>
+            <h3>{{ etc.title }}</h3>
+          </li>
+          <li v-for="(skill, index) in etc.skills" :key="index">
+            <p class="skill-name" lang="en">{{ skill.name }}</p>
           </li>
         </ul>
       </li>
@@ -65,27 +80,38 @@ export default {
       // 프론트엔드
       front: {
         title: 'Frontend',
+        icon: 'src/assets/image/skill/front_icon.svg',
+        alt: '',
         skills: [
-          { name: 'HTML', level: 90, type: 'markup' },
-          { name: 'CSS/SCSS', level: 90, type: 'style' },
-          { name: 'JavaScript & jQuery', level: 75, type: 'programming' },
+          { name: 'Html', level: 90, type: 'markup' },
+          { name: 'Css/Scss', level: 90, type: 'style' },
+          { name: 'Js & jQ', level: 75, type: 'programming' },
           { name: 'Vue', level: 70, type: 'framework' },
-          { name: 'SEO', level: 60, type: 'concept' },
+          { name: 'Seo', level: 60, type: 'concept' },
           { name: 'Semantic', level: 65, type: 'concept' },
-          { name: 'PHP', level: 30, type: 'backend' },
+          { name: 'Php', level: 30, type: 'backend' },
         ],
       },
       // 디자인
       design: {
         title: 'Design',
+        icon: 'src/assets/image/skill/design_icon.svg',
+        alt: '',
+        skills: [{ name: 'Photoshop' }, { name: 'Figma' }],
       },
       // 협업툴
       collaboration: {
         title: 'Collaboration',
+        icon: 'src/assets/image/skill/collaboration_icon.svg',
+        alt: '',
+        skills: [{ name: 'GitHub & GitLab' }, { name: 'Jira' }],
       },
       // 기타
       etc: {
         title: 'Etc',
+        icon: 'src/assets/image/skill/etc_icon.svg',
+        alt: '',
+        skills: [{ name: 'React' }],
       },
     }
   },
@@ -95,7 +121,7 @@ export default {
 <style lang="scss">
 #skill {
   width: 90%;
-  margin: 0 auto;
+  margin: 0 auto 25rem;
   h1 {
     padding: 145px 0 35px 0;
     text-align: center;
@@ -115,33 +141,63 @@ export default {
   .skills-box {
     display: flex;
     justify-content: space-evenly;
+    gap: 2rem;
     li {
       display: flex;
-      width: calc(100% / 3);
+      width: calc(100% / 4);
+      align-items: flex-start;
     }
-    .front-skill {
+    .skill__inner {
       .skill-list {
         width: 100%;
-        margin-right: 5rem;
-        &:nth-of-type(3) {
-          margin-right: 0;
-        }
+
         > li {
           margin-bottom: 15px;
           display: flex;
           width: 100%;
+          &:first-child {
+            flex-direction: column;
+            h3 {
+              font-size: 2.1rem;
+              font-weight: 600;
+              margin-bottom: 1.5rem;
+            }
+            .icon-box {
+              padding: 1.3rem;
+              border-radius: 0.8rem;
+              margin-bottom: 1.5rem;
+              width: 60px;
+              height: 60px;
+              display: flex;
+              justify-content: center;
+
+              &.front {
+                background: linear-gradient(133deg, #faab89 0%, #f54a00 100%);
+              }
+              &.design {
+                background: linear-gradient(133deg, #dfbfff 0%, #c084fc 100%);
+              }
+
+              &.collaboration {
+                background: linear-gradient(133deg, #709cff 0%, #155dfc 100%);
+              }
+              &.etc {
+                background: linear-gradient(133deg, #2f54ce 0%, #9071ff 100%);
+              }
+            }
+          }
 
           .skill-name {
             font-weight: 200;
-            margin-bottom: 10px;
+            margin-bottom: 1rem;
             width: 200px;
             color: $dec;
             font-size: 1.7rem;
           }
 
           .skill-level {
-            background: #333;
-            border-radius: 10px;
+            background: #33333370;
+            border-radius: 1rem;
             overflow: hidden;
             height: 20px;
             display: flex;
@@ -151,7 +207,7 @@ export default {
               width: 0;
               height: 100%;
               background: linear-gradient(90deg, #ff9966, #822d30);
-              border-radius: 10px;
+              border-radius: 1rem;
               display: flex;
               align-items: center;
               justify-content: flex-end;
@@ -167,7 +223,25 @@ export default {
           }
         }
       }
+
+      &.design-skill,
+      &.collaboration-skill,
+      &.etc-skill {
+        .skill-list {
+          .skill-name {
+            width: 100%;
+            background-color: #1e2939;
+            padding: 0.9rem 1.8rem;
+            border-radius: 1rem;
+          }
+        }
+      }
     }
+  }
+  .glass {
+    @include glass(100%, 485px, 4px, 180%, 2rem, #fff);
+    border-radius: 30px;
+    padding: 3.5rem 3rem;
   }
 }
 
